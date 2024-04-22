8cced4879c3d2a08bac2dddef443b8e1b191c96e
Saiful Rijal <46024788+saifulrijal-ds@users.noreply.github.com>
2024-04-22 18:15:29 +0000

    Update visualization

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.set_page_config(layout='wide')
 st.title("Customer Analysis")
 st.header("RFM and Quartile Analysis")
 
@@ -50,12 +51,59 @@ df = load_data()
 st.subheader("Dataset")
 st.dataframe(df)
 
+def plot_recency_distribution():
+    st.subheader("Recency : Number of days since customer's last purchase")
+    fig = px.histogram(df, x='Recency')
+    st.plotly_chart(fig)
+
+st.header("Recency")
+plot_recency_distribution()
+
+st.header("Frequency")
+freq_col_1, freq_col_2 = st.columns(2)
+
+@st.experimental_fragment
+def plot_frequency_histogram():
+    st.subheader("Distribution of number of purchases")
+    selected_freq_column = st.selectbox("Select a column", 
+                                        ["NumWebPurchases", 
+                                         "NumCatalogPurchases", 
+                                         "NumStorePurchases", 
+                                         "NumAllPurchases_Frequency"])
+    fig = px.histogram(df, x=selected_freq_column)
+    st.plotly_chart(fig)
+
+with freq_col_1:
+    plot_frequency_histogram()
+
+def plot_freq_corr():
+    st.subheader("Correlation between places (number of purchases)")
+    corr_matrix = df[[
+        "NumWebPurchases", 
+        "NumCatalogPurchases", 
+        "NumStorePurchases"
+    ]].corr(method='spearman')
+    fig = px.imshow(corr_matrix, text_auto=True)
+    st.plotly_chart(fig)
+
+with freq_col_2:
+    plot_freq_corr()
+
+st.header("Monetary")
 product_col_1, product_col_2 = st.columns(2)
 
 @st.experimental_fragment
 def plot_product_histogram():
-    st.subheader("Distribution of spent amount for each product")
-    selected_product_column = st.selectbox("Select a column", [x for x in df.columns if 'mnt' in x.lower()])
+    st.subheader("Distribution of spent amounts.")
+    selected_product_column = st.selectbox("Select a column", [
+        "MntWines",
+        "MntFruits",
+        'MntMeatProducts', 
+        'MntFishProducts', 
+        'MntSweetProducts',
+        'MntGoldProds',
+        "MntAllProducts_Monetary"
+    ])
     fig = px.histogram(df, x=selected_product_column)
     st.plotly_chart(fig)
 
@@ -64,7 +112,14 @@ with product_col_1:
 
 def plot_product_corr():
     st.subheader("Correlation between products")
-    corr_matrix = df[[x for x in df.columns if 'mnt' in x.lower()]].corr(method='spearman')
+    corr_matrix = df[[
+        "MntWines",
+        "MntFruits",
+        'MntMeatProducts', 
+        'MntFishProducts', 
+        'MntSweetProducts',
+        'MntGoldProds'
+    ]].corr(method='spearman')
     fig = px.imshow(corr_matrix, text_auto=True)
     st.plotly_chart(fig)
 
